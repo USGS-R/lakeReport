3f6c60e6b27aed7aad6f5b08ec9348dd517005e7
Lindsay Carr <lcarr@usgs.gov>
2016-02-03 12:37:52 -0600

    moved to creating pdf/word docs for each report

--- workflow_new.R
+++ workflow_new.R
@@ -1,76 +1,49 @@
-library(dplyr)
-library(dataRetrieval)
-library(tidyr)
-library(gsplot)
+# 
+# pcodes <- read.csv("pcodes.lakes1",header=FALSE,
+#                    colClasses = "character")[,1]
+# 
+# sites <- read.csv("water_quality_lakes",header=FALSE,
+#                   colClasses = "character")[,1]
+# 
+# sitesStage <- read.csv("stage_only_lakes",header=FALSE,
+#                        colClasses = "character")[,1]
+# 
+# qwDataNWIS <- readNWISqw(sites, pcodes, "2013-10-01", "2014-09-30")
+# qwDataWQP <- readWQPqw(paste0("USGS-",sites), pcodes, "2013-10-01", "2014-09-30")
 
-pcodes <- read.csv("pcodes.lakes1",header=FALSE,
-                   colClasses = "character")[,1]
+#####################################################################################
 
-sites <- read.csv("water_quality_lakes",header=FALSE,
-                  colClasses = "character")[,1]
+## generating PDFs workflow
 
-sitesStage <- read.csv("stage_only_lakes",header=FALSE,
-                       colClasses = "character")[,1]
+renderLakeReports <- function(reportName, siteNumber, wy, output){
+  library(rmarkdown)
+  
+  output_dir <- getwd()
+  filename <- paste(reportName, wy, siteNumber, sep="_")
+  rmd_file <- paste0(reportName, '.Rmd') 
+  
+  out_file <- render(rmd_file, paste0(output,"_document"), 
+                     output_file = paste(filename, output, sep="."),
+                     output_dir = output_dir)
+  return(out_file)
+}
 
-qwDataNWIS <- readNWISqw(sites, pcodes, "2013-10-01", "2014-09-30")
-qwDataWQP <- readWQPqw(paste0("USGS-",sites), pcodes, "2013-10-01", "2014-09-30")
+#example workflow:
+
+##rendering as pdf
+renderLakeReports('ghtable', '05390500', 2014, 'pdf')
+renderLakeReports('stagehydrograph', '05390500', 2014, 'pdf')
+renderLakeReports('qwtable', "455638089034501", 2014, 'pdf')
+renderLakeReports('depthprofiles', "455638089034501", 2014, 'pdf')
+renderLakeReports('qwtimeseries', "455638089034501", 2014, 'pdf')
+
+## rendering as word docs (tables don't have nice formatting)
+renderLakeReports('ghtable', '05390500', 2014, 'word')
+renderLakeReports('stagehydrograph', '05390500', 2014, 'word')
+renderLakeReports('qwtable', "455638089034501", 2014, 'word')
+renderLakeReports('depthprofiles', "455638089034501", 2014, 'word')
+renderLakeReports('qwtimeseries', "455638089034501", 2014, 'word')
+
+#####################################################################################
 
 
-
-### gh table & stage hydrograph, p.24 ###
-
-library(dataRetrieval)
-library(lubridate)
-library(dplyr)
-library(tidyr)
-library(gsplot)
-
-wy_start <- as.POSIXct("2013-10-01")
-wy_end <- as.POSIXct("2014-09-30")
-
-stage_data <- readNWISdata(service = "dv", sites = "05390500", 
-                           parameterCd = "00065", startDate = "1800-01-01", 
-                           endDate = wy_end)
-
-ghtable <- makeGageHeightTable(stage_data, wy_start) 
-stagehydro <- makeStageHydrograph(stage_data)
-
-### ###
-
-### qw data table & lake profiles, p.25-29 ###
-library(dataRetrieval)
-library(dplyr)
-library(gsplot)
-
-wy_start <- as.POSIXct("2013-10-01")
-wy_end <- as.POSIXct("2014-09-30")
-
-pcodes <- read.csv("pcodes.lakes1",header=FALSE,
-                   colClasses = "character")[,1]
-
-qw_nwis <- readNWISqw(siteNumbers = "455638089034501", 
-                      parameterCd = pcodes, 
-                      startDate = wy_start, 
-                      endDat = wy_end)
-
-qw_table <- makeWqTable(qw_nwis)
-qw_depth_profiles <- makeLakeDepthProfiles(qw_nwis)
-
-### ###
-
-### qw timeseries, p. 30 ###
-library(dataRetrieval)
-library(gsplot)
-
-wy_end <- as.POSIXct("2014-09-30")
-
-pcodes <- c("00665", "00666", "32210", "00078")
-
-qw_historic <- readNWISqw(siteNumbers = "455638089034501", 
-                          parameterCd = pcodes,
-                          startDate = "1800-01-01", 
-                          endDat = wy_end)
-
-qw_timeseries <- makeTimeseriesPlots(qw_historic)
-
-### ###
