9e77348208c9a9367bc2b03fd06746cce4f9ae58
Lindsay Carr <lcarr@usgs.gov>
2016-02-16 16:22:07 -0600

    stagehydro & ghtable: added another column name to ignore

--- R/functions-ghtable.R
+++ R/functions-ghtable.R
@@ -5,7 +5,8 @@ makeGageHeightTable <- function(stage_data, wy_start){
   
   gh_table <- stage_data %>% 
     filter(dateTime >= wy_start) %>% 
-    select(dateTime, ends_with("_00065_00003"), -starts_with("X_OBSERVER")) %>% 
+    select(dateTime, ends_with("_00065_00003"), 
+           -starts_with("X_OBSERVER"), -starts_with('X_TAILWATER')) %>% 
     mutate(Month = factor(month(dateTime, label = TRUE, abbr = TRUE), 
                           levels = wy_months, ordered = TRUE)) 
   
